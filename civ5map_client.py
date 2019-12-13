19ab64af24a2f0c14eb40aad67d9330c8804b039
F. Chan <edu.fechan@gmail.com>
2019-12-12 17:31:48 -0800

    Update client

--- civ5map_client.py
+++ civ5map_client.py
@@ -5,7 +5,7 @@ map = civ5map.Civ5map.from_file(path)
 terrains = map.header.terrain_list.values
 for row in map.mapdata.plot_matrix[::-1]:
     for plot in row.plot_list:
-        if terrains[int.from_bytes(plot.terrain_type_id, "little")] == "TERRAIN_OCEAN" or terrains[int.from_bytes(plot.terrain_type_id, "little")] == "TERRAIN_COAST":
+        if terrains[plot.terrain_type_id] == "TERRAIN_OCEAN" or terrains[plot.terrain_type_id] == "TERRAIN_COAST":
             print(" ", end='')
         else:
             print("#", end='')
